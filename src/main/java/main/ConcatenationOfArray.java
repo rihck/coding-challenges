73769a95861721031940fc948b38964813c36426
rhenrique <rhenrique@squaretrade.com>
2022-08-04 16:47:18 -0300

    ConcatenationOfArray

---
+++ src/main/java/main/ConcatenationOfArray.java
@@ -0,0 +1,39 @@
+package main;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+// https://leetcode.com/problems/concatenation-of-array/
+public class ConcatenationOfArray {
+    public static int[] getConcatenation(int[] nums) {
+        int[] output = new int[nums.length * 2];
+
+        for (int i = 0; i < nums.length; i++){
+            output[i] = nums[i];
+            output[i + nums.length] = nums[i];
+        }
+
+        return output;
+    }
+
+    @Test
+    void defaultCase(){
+        int[] input = {1, 2, 1};
+        int[] output = {1, 2, 1, 1, 2, 1};
+
+        int[] concatenation = getConcatenation(input);
+
+        assertArrayEquals(concatenation, output);
+    }
+
+    public static void main(String[] args) {
+        int[] input = {1, 2, 1};
+        int[] output = new int[input.length * 2];
+
+        for (int i = 0; i < input.length; i++){
+            output[i] = input[i];
+            output[i] = input[i + input.length -1];
+        }
+
+    }
+}
