29984c0dbf4d56b4981018823ae9865970a4edd1
rhenrique <rhenrique@squaretrade.com>
2022-08-11 14:30:13 -0300

    HasPairWithSum with NOT Ordered Input

--- src/main/java/main/HasPairWithSum.java
+++ src/main/java/main/HasPairWithSum.java
@@ -3,7 +3,9 @@ package main;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,6 +19,20 @@ public class HasPairWithSum {
 
         System.out.println(resultWithMatch);
         System.out.println(resultWithoutMatch);
+    }
+
+    static boolean hasPairWithSumNotOrdered(List<Integer> ordered, int sumTarget){
+
+        Set<Integer> previousElementsSum = new HashSet<>();
+
+        for (int i = 0; i < ordered.size(); i++){
+            if (previousElementsSum.contains(ordered.get(i)))
+                return true;
+
+            previousElementsSum.add(sumTarget - ordered.get(i));
+        }
+
+        return false;
     }
 
     static boolean hasPairWithSumOrdered(List<Integer> ordered, int sumTarget){
@@ -38,11 +54,10 @@ public class HasPairWithSum {
         }
 
         return false;
-
     }
 
     @Test
-    void testDefault(){
+    void testAssumingOrdered(){
         boolean resultWithoutMatch = hasPairWithSumOrdered(Arrays.asList(1, 2, 3, 9), 8);
         boolean resultWithMatch = hasPairWithSumOrdered(Arrays.asList(1, 2, 4, 4), 8);
 
@@ -50,5 +65,13 @@ public class HasPairWithSum {
         assertFalse(resultWithoutMatch);
     }
 
+    @Test
+    void testAssumingNotOrdered(){
+        boolean resultWithoutMatch = hasPairWithSumNotOrdered(Arrays.asList(1, 2, 3, 9), 8);
+        boolean resultWithMatch = hasPairWithSumNotOrdered(Arrays.asList(1, 2, 4, 4), 8);
+
+        assertTrue(resultWithMatch);
+        assertFalse(resultWithoutMatch);
+    }
 
 }
