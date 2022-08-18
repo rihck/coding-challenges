3efb7b779dfe7817ebc3ce68fd729eea52c3a4d9
Ricardo <ricardophp29@gmail.com>
2022-08-18 14:00:18 -0300

    BuildBinarySearchTree

---
+++ src/main/java/main/BuildBinarySearchTree.java
@@ -0,0 +1,91 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Build BST structure and have the following structure
+ *            9
+ *      4           20
+ *   1    6      15    170
+ */
+public class BuildBinarySearchTree {
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+        tree.insert(9);
+        tree.insert(4);
+        tree.insert(6);
+        tree.insert(20);
+        tree.insert(170);
+        tree.insert(15);
+        tree.insert(1);
+
+        tree.traverse(tree.root);
+        System.out.println(tree);
+    }
+
+    static class BinarySearchTree{
+        Node root;
+
+        Node insert(int valueToInsert){
+            Node newNode = new Node(valueToInsert);
+            if (root == null){
+                this.root = newNode;
+                return newNode;
+            }
+
+            Node currentNode = this.root;
+
+            while (currentNode != null){
+                int currentNodeValue = currentNode.value;
+
+                //PS: It's up to us if we want to put equal values on LEFT or RIGHT
+                if (valueToInsert > currentNodeValue){
+                    //Right
+                    if (currentNode.right != null){
+                        currentNode = currentNode.right;
+                    }
+                    else {
+                        currentNode.right = newNode;
+                        currentNode = null;
+                    }
+                }
+
+                else { //valueToInsert <= currentNodeValue
+                    //Left
+                    if (currentNode.left != null){
+                        currentNode = currentNode.left;
+
+                    } else {
+                        currentNode.left = newNode;
+                        currentNode = null;
+                    }
+                }
+            }
+
+            return newNode;
+        }
+
+        void traverse(Node node){
+            System.out.println(node.value);
+
+            if (node.right != null){
+                traverse(node.right);
+            }
+
+            if (node.left != null) {
+                traverse(node.left);
+            }
+        }
+    }
+    static class Node{
+        Node left;
+        Node right;
+        int value;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+}
