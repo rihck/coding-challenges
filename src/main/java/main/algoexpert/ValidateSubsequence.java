e095cc50dc17a344355cc841f34dce294e22d69d
Henry <ricardophp29@gmail.com>
2023-03-08 11:07:20 -0300

    ValidateSubsequence - Simpler and right solution

--- src/main/java/main/algoexpert/ValidateSubsequence.java
+++ src/main/java/main/algoexpert/ValidateSubsequence.java
@@ -1,44 +1,42 @@
 package main.algoexpert;
 
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ValidateSubsequence {
 
     public boolean validateSubSequence(List<Integer> array, List<Integer> sequence){
-        Map<Integer, Integer> valuesAndIndexes = new HashMap<>();
+        int indexSubSequence = 0;
+        boolean isSubSequence = false;
         for(int i = 0; i < array.size(); i++){
-            valuesAndIndexes.put(array.get(i), i);
-        }
-
-        int lastIndex = -1;
-        for (int i = 0; i < sequence.size(); i++){
-            if (!valuesAndIndexes.containsKey(sequence.get(i))){
-                return false;
+            if (array.get(i) == sequence.get(indexSubSequence)){
+                indexSubSequence++;
             }
 
-            int foundNumberIndex = valuesAndIndexes.get(sequence.get(i));
-            if (lastIndex > foundNumberIndex)
-                return false;
-
-            lastIndex = foundNumberIndex;
+            if (indexSubSequence >= sequence.size()){
+                isSubSequence = true;
+                break;
+            }
         }
 
-        return true;
+        return isSubSequence;
     }
 
     @Test
     @DisplayName("Contains Partial Sub Sequence")
     public void containsPartialSubSequence(){
         assertTrue(validateSubSequence(Arrays.asList(1, 7, -1, 10), Arrays.asList(1, -1)));
+    }
+
+    @Test
+    @DisplayName("Contains Sub Sequence but last match in last element")
+    public void containsSubSequenceButInLastElement(){
+        assertFalse(validateSubSequence(Arrays.asList(5, 1, 22, 25, 6, -1, 8, 10), Arrays.asList(1, 6, -1, 10)));
     }
 
     @Test
