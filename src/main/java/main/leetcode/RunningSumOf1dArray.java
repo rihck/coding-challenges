bd6d910ec9d805abe86cd0651e61dea6c0ebaccd
Henry <ricardophp29@gmail.com>
2023-06-16 10:14:47 -0300

    [LC] - RunningSumOf1dArray

---
+++ src/main/java/main/leetcode/RunningSumOf1dArray.java
@@ -0,0 +1,14 @@
+package main.leetcode;
+
+public class RunningSumOf1dArray {
+    public int[] runningSum(int[] nums) {
+        int[] sums = new int[nums.length];
+        sums[0] = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            sums[i] = sums[i-1] + nums[i];
+        }
+
+        return sums;
+    }
+}
