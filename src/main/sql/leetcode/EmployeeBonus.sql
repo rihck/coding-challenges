af2e4e9f91d2dce305c511c92979f9ee97f02a58
Henry <ricardophp29@gmail.com>
2023-07-02 14:34:36 -0300

    [LC] (SQL) -  Employee Bonus

--- src/main/sql/leetcode/EmployeeBonus.sql
+++ src/main/sql/leetcode/EmployeeBonus.sql
@@ -1,2 +1,8 @@
 -- https://leetcode.com/problems/employee-bonus/description/
 
+-- LEFT JOIN filtering by NULL and < 1000
+SELECT e.name name, b.bonus bonus
+FROM Employee e
+LEFT JOIN Bonus b ON (e.empId = b.empId)
+WHERE (b.bonus IS NULL OR b.bonus < 1000)
+
