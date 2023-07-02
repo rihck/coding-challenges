e6e5ac1c4a2eb125916e42627c86af3dd8a10479
Henry <ricardophp29@gmail.com>
2023-07-02 14:55:16 -0300

    [LC] (SQL) - Find Customer Referee: Another approach II

--- src/main/sql/leetcode/FindCustomerReferee.sql
+++ src/main/sql/leetcode/FindCustomerReferee.sql
@@ -11,3 +11,13 @@ WHERE IFNULL(referee_id, 0) != 2
 SELECT name
 FROM Customer
 WHERE referee_id != 2 OR referee_id IS NULL
+
+
+-- SubQuery Approach
+SELECT name
+FROM Customer
+WHERE id NOT IN (
+    SELECT id
+    FROM Customer
+    WHERE referee_id = 2
+)
