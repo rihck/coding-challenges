eba2e569b0e623341c0e1a1722aabbb50cb65535
Henry <ricardophp29@gmail.com>
2023-07-02 14:51:45 -0300

    [LC] (SQL) - Find Customer Referee: Another approach

--- src/main/sql/leetcode/FindCustomerReferee.sql
+++ src/main/sql/leetcode/FindCustomerReferee.sql
@@ -6,3 +6,8 @@ FROM Customer
 WHERE IFNULL(referee_id, 0) != 2
 -- -- IF the Bonus is NULL, it's "converted" to 0 than the competition part is done
 
+
+-- Replacing IF NULL
+SELECT name
+FROM Customer
+WHERE referee_id != 2 OR referee_id IS NULL
