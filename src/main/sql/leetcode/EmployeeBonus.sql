77be9818f009476459b410fc4d7ffcba83a2be41
Henry <ricardophp29@gmail.com>
2023-07-02 14:44:45 -0300

    [LC] (SQL) - Employee Bonus: Other approach

--- src/main/sql/leetcode/EmployeeBonus.sql
+++ src/main/sql/leetcode/EmployeeBonus.sql
@@ -6,3 +6,10 @@ FROM Employee e
 LEFT JOIN Bonus b ON (e.empId = b.empId)
 WHERE (b.bonus IS NULL OR b.bonus < 1000)
 
+
+-- IF Null Approach (instead of IS NULL)
+SELECT e.name name, b.bonus bonus
+FROM Employee e
+LEFT JOIN Bonus b ON (e.empId = b.empId)
+WHERE IFNULL(bonus, 0) < 1000
+-- IF the Bonus is NULL, it's "converted" to 0 than the competition part is done
