436146db2aab1b119fb50d6648862de3c2b351c0
Henry <ricardophp29@gmail.com>
2023-07-03 17:19:45 -0300

    [LC] (SQL) - Customer Placing the Largest Number of Orders: Handling multiple customers with same highest orders

--- src/main/sql/leetcode/CustomerPlacingTheLargestNumberOfOrders.sql
+++ src/main/sql/leetcode/CustomerPlacingTheLargestNumberOfOrders.sql
@@ -1,7 +1,21 @@
 -- https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/
 
+-- Return the customer with most orders (Doesn't handle multiple customers with same highest orders)
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
 ORDER BY COUNT(order_number) DESC
 LIMIT 1
+
+-- Return the customerS (HANDLE multiple customers with same highest orders)
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+HAVING COUNT(order_number) =
+       (
+           SELECT count(order_number)
+           FROM Orders
+           GROUP BY customer_number
+           ORDER BY COUNT(customer_number) DESC
+           LIMIT 1
+    )
