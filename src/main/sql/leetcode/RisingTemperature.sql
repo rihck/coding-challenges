0d78be5a0960a10a9aca7385c1ba01f885ac89d3
Henry <ricardophp29@gmail.com>
2023-07-04 12:14:00 -0300

    [LC] (SQL) - RisingTemperature.sql adding Schema and I/O

--- src/main/sql/leetcode/RisingTemperature.sql
+++ src/main/sql/leetcode/RisingTemperature.sql
@@ -1,3 +1,5 @@
+-- https://leetcode.com/problems/rising-temperature/
+
 --## Self Join using DateDiff to compare: records that has 1 day difference betweem them selecting the one with higher temperature
 SELECT w1.id,
 FROM Weather w1
@@ -13,3 +15,42 @@ WHERE w1.temperature > w2.temperature
 --    # Some observations:
 --    ## Subdate (subdate(w1.recordDate, 1)) function that subtracts X days from that Date
 --    ## DATE_ADD(w2.recordDate, INTERVAL 1 DAY) function that ADDs intervals to the date (DAY / MONTH / YEAR)
+
+/** Schema
+    Table: Weather
+
+    +---------------+---------+
+    | Column Name   | Type    |
+    +---------------+---------+
+    | id            | int     |
+    | recordDate    | date    |
+    | temperature   | int     |
+    +---------------+---------+
+    id is the primary key for this table.
+    This table contains information about the temperature on a certain day.
+*/
+
+---------
+
+/** I/O
+    Input:
+    Weather table:
+    +----+------------+-------------+
+    | id | recordDate | temperature |
+    +----+------------+-------------+
+    | 1  | 2015-01-01 | 10          |
+    | 2  | 2015-01-02 | 25          |
+    | 3  | 2015-01-03 | 20          |
+    | 4  | 2015-01-04 | 30          |
+    +----+------------+-------------+
+    Output:
+    +----+
+    | id |
+    +----+
+    | 2  |
+    | 4  |
+    +----+
+    Explanation:
+    In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+    In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+*/
