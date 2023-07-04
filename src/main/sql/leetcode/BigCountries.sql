8a262540873b34515166a57e3088c5def2bc419f
Henry <ricardophp29@gmail.com>
2023-07-04 11:45:49 -0300

    [LC] (SQL) - Big Countries: Faster solution using UNION -> Explanation

--- src/main/sql/leetcode/BigCountries.sql
+++ src/main/sql/leetcode/BigCountries.sql
@@ -17,3 +17,15 @@ SELECT name, population, area
 FROM World
 WHERE population > 25000000
 
+/* Explanation source: https://leetcode.com/problems/big-countries/solutions/103561/union-and-or-and-the-explanation/
+ Why Union is faster than OR?
+
+Strictly speaking, Using UNION is faster when it comes to cases like scan two different column like this.
+
+(Of course using UNION ALL is much faster than UNION since we don't need to sort the result. But it violates the requirements)
+
+Suppose we are searching population and area, Given that MySQL usually uses one one index per table in a given query, so when it uses the 1st index rather than 2nd index, it would still have to do a table-scan to find rows that fit the 2nd index.
+
+When using UNION, each sub-query can use the index of its search, then combine the sub-query by UNION.
+
+ */
