435d8e10fc175c390da24a997ab4d6d620bb16a7
Henry <ricardophp29@gmail.com>
2023-07-04 12:05:08 -0300

    [LC] (SQL) - CustomersWhoNeverOrder.sql adding Schema and I/O

--- src/main/sql/leetcode/CustomersWhoNeverOrder.sql
+++ src/main/sql/leetcode/CustomersWhoNeverOrder.sql
@@ -13,3 +13,28 @@ WHERE o.customerId IS NULL
 SELECT c.name Customers
 FROM Customers C
 WHERE c.id NOT IN (SELECT customerId from Orders)
+
+/** Schema
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | id          | int     |
+    | name        | varchar |
+    +-------------+---------+
+    id is the primary key column for this table.
+    Each row of this table indicates the ID and name of a customer.
+*/
+
+---------
+
+/** I/O
+    +-------------+------+
+    | Column Name | Type |
+    +-------------+------+
+    | id          | int  |
+    | customerId  | int  |
+    +-------------+------+
+    id is the primary key column for this table.
+    customerId is a foreign key of the ID from the Customers table.
+    Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+*/
