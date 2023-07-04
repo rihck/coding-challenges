75d91211a4a81ca9100f3317b6dd91157e2eb713
Henry <ricardophp29@gmail.com>
2023-07-04 12:07:12 -0300

    [LC] (SQL) - DuplicateEmails.sql adding Schema and I/O

--- src/main/sql/leetcode/DuplicateEmails.sql
+++ src/main/sql/leetcode/DuplicateEmails.sql
@@ -9,3 +9,37 @@ INNER JOIN Person p2 ON (p1.email = p2.email and p1.id <> p2.id)
 SELECT Email
 FROM Person
 GROUP BY Email HAVING count(Email) > 1
+
+/** Schema
+    Table: Person
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | id          | int     |
+    | email       | varchar |
+    +-------------+---------+
+    id is the primary key column for this table.
+    Each row of this table contains an email. The emails will not contain uppercase letters.
+*/
+
+---------
+
+/** I/O
+    Input:
+    Person table:
+    +----+---------+
+    | id | email   |
+    +----+---------+
+    | 1  | a@b.com |
+    | 2  | c@d.com |
+    | 3  | a@b.com |
+    +----+---------+
+    Output:
+    +---------+
+    | Email   |
+    +---------+
+    | a@b.com |
+    +---------+
+    Explanation: a@b.com is repeated two times.
+*/
