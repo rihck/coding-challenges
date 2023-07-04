7f6f12a537660172e3859611f02cfb803c781515
Henry <ricardophp29@gmail.com>
2023-07-04 12:15:14 -0300

    [LC] (SQL) - BigCountries.sql adding Schema and I/O

--- src/main/sql/leetcode/BigCountries.sql
+++ src/main/sql/leetcode/BigCountries.sql
@@ -29,3 +29,42 @@ Suppose we are searching population and area, Given that MySQL usually uses one 
 When using UNION, each sub-query can use the index of its search, then combine the sub-query by UNION.
 
  */
+
+/** Schema
+    Table: World
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | name        | varchar |
+    | continent   | varchar |
+    | area        | int     |
+    | population  | int     |
+    | gdp         | bigint  |
+    +-------------+---------+
+    name is the primary key column for this table.
+    Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+*/
+
+---------
+
+/** I/O
+    Input:
+    World table:
+    +-------------+-----------+---------+------------+--------------+
+    | name        | continent | area    | population | gdp          |
+    +-------------+-----------+---------+------------+--------------+
+    | Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+    | Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+    | Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+    | Andorra     | Europe    | 468     | 78115      | 3712000000   |
+    | Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
+    +-------------+-----------+---------+------------+--------------+
+    Output:
+    +-------------+------------+---------+
+    | name        | population | area    |
+    +-------------+------------+---------+
+    | Afghanistan | 25500100   | 652230  |
+    | Algeria     | 37100000   | 2381741 |
+    +-------------+------------+---------+
+*/
