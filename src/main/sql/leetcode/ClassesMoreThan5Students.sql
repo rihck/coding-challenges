8b861c4fe371984167d0ed26a2fc14dc8179b6ed
Henry <ricardophp29@gmail.com>
2023-07-04 11:59:46 -0300

    [LC] (SQL) - Classes More Than 5 Students: Schema + Explanation

--- src/main/sql/leetcode/ClassesMoreThan5Students.sql
+++ src/main/sql/leetcode/ClassesMoreThan5Students.sql
@@ -1,7 +1,53 @@
 -- https://leetcode.com/problems/classes-more-than-5-students/
 
 -- Group by solution:
+/**
+  Group by classes
+  Filter by grouped > 5
+ */
 SELECT class
 FROM Courses
 GROUP BY class
 HAVING count(*) > 5
+
+/** Schema
+  +-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key column for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ */
+
+
+/**
+ Input:
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output:
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation:
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+
+ */
