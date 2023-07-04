6007a2cd3d93c2cd815a6e181837c8172145eca7
Henry <ricardophp29@gmail.com>
2023-07-04 11:45:03 -0300

    [LC] (SQL) - Big Countries: Faster solution using UNION

--- src/main/sql/leetcode/BigCountries.sql
+++ src/main/sql/leetcode/BigCountries.sql
@@ -5,3 +5,15 @@ SELECT name, population, area
 FROM World
 WHERE area >= 3000000 OR population >= 25000000
 
+
+-- Using Union (faster)
+SELECT name, population, area
+FROM World
+WHERE area > 3000000
+
+UNION
+
+SELECT name, population, area
+FROM World
+WHERE population > 25000000
+
