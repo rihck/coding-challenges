2a5f6381f58c1304bb419ca669ba4906c625534b
Henry <ricardophp29@gmail.com>
2023-07-04 12:12:00 -0300

    [LC] (SQL) - FindCustomerReferee.sql adding Schema and I/O

--- src/main/sql/leetcode/FindCustomerReferee.sql
+++ src/main/sql/leetcode/FindCustomerReferee.sql
@@ -21,3 +21,43 @@ WHERE id NOT IN (
     FROM Customer
     WHERE referee_id = 2
 )
+
+/** Schema
+    Table: Customer
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | id          | int     |
+    | name        | varchar |
+    | referee_id  | int     |
+    +-------------+---------+
+    id is the primary key column for this table.
+    Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+*/
+
+---------
+
+/** I/O
+    Input:
+    Customer table:
+    +----+------+------------+
+    | id | name | referee_id |
+    +----+------+------------+
+    | 1  | Will | null       |
+    | 2  | Jane | null       |
+    | 3  | Alex | 2          |
+    | 4  | Bill | null       |
+    | 5  | Zack | 1          |
+    | 6  | Mark | 2          |
+    +----+------+------------+
+    Output:
+    +------+
+    | name |
+    +------+
+    | Will |
+    | Jane |
+    | Bill |
+    | Zack |
+    +------+
+*/
