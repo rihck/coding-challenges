22efc5e7440855fbb76361492e4bdc51cd466723
Henry <ricardophp29@gmail.com>
2023-07-05 13:35:57 -0300

    [LC] (ALGO) - ValidParentheses: Best solution using Stack

--- src/main/java/main/leetcode/ValidParentheses.java
+++ src/main/java/main/leetcode/ValidParentheses.java
@@ -5,10 +5,37 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 
 /** https://leetcode.com/problems/valid-parentheses/ **/
 public class ValidParentheses {
-    public boolean isValid(String s) {
+    public boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+
+    @Test
+    public void mainTestCases(){
+        Assert.assertTrue(isValid("()"));
+        Assert.assertTrue(isValid("()[]{}"));
+        Assert.assertTrue(isValid("([{}])"));
+
+        Assert.assertFalse(isValid("(]"));
+
+        Assert.assertFalse(isValid("([)]"));
+    }
+
+    public boolean isValidFirstApproach(String s) {
         Map<String, Integer> simbolsCount = new HashMap<>();
         simbolsCount.put("(", 0);
         simbolsCount.put(")", 0);
@@ -35,12 +62,12 @@ public class ValidParentheses {
     }
 
     @Test
-    public void mainTestCases(){
-        Assert.assertTrue(isValid("()"));
-        Assert.assertTrue(isValid("()[]{}"));
-        Assert.assertFalse(isValid("(]"));
+    public void mainTestCasesFirstApproach(){
+        Assert.assertTrue(isValidFirstApproach("()"));
+        Assert.assertTrue(isValidFirstApproach("()[]{}"));
+        Assert.assertFalse(isValidFirstApproach("(]"));
 
         // Failing at first attempt solution
-        Assert.assertFalse(isValid("([)]"));
+        //Assert.assertFalse(isValidFirstApproach("([)]"));
     }
 }
