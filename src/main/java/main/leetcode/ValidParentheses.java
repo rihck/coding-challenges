79a97f9e063cdc6a8d0b1279c55d65686c2bf726
Henry <ricardophp29@gmail.com>
2023-07-05 12:33:39 -0300

    [LC] (ALGO) - ValidParentheses: First solution

--- src/main/java/main/leetcode/ValidParentheses.java
+++ src/main/java/main/leetcode/ValidParentheses.java
@@ -1,8 +1,43 @@
 package main.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /** https://leetcode.com/problems/valid-parentheses/ **/
 public class ValidParentheses {
     public boolean isValid(String s) {
+        Map<String, Integer> simbolsCount = new HashMap<>();
+        simbolsCount.put("(", 0);
+        simbolsCount.put(")", 0);
+        simbolsCount.put("[", 0);
+        simbolsCount.put("]", 0);
+        simbolsCount.put("{", 0);
+        simbolsCount.put("}", 0);
+
+        for (int i = 0; i < s.length(); i++){
+            String currentChar = String.valueOf(s.charAt(i));
+            simbolsCount.put(currentChar, simbolsCount.get(currentChar) + 1);
+        }
+
+        if (simbolsCount.get("(") != simbolsCount.get(")"))
+            return false;
+
+        if (simbolsCount.get("[") != simbolsCount.get("]"))
+            return false;
+
+        if (simbolsCount.get("{") != simbolsCount.get("}"))
+            return false;
+
+        return true;
     }
 
+    @Test
+    public void mainTestCases(){
+        Assert.assertTrue(isValid("()"));
+        Assert.assertTrue(isValid("()[]{}"));
+        Assert.assertFalse(isValid("(]"));
+    }
 }
