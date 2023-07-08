1dc181c24d076b162c6b31d591a1752a58fbc3e0
Henry <ricardophp29@gmail.com>
2023-07-08 08:18:06 -0300

    [LC] (SQL) - Triangle Judgement: Schema and I/O

--- src/main/sql/leetcode/TriangleJudgement.sql
+++ src/main/sql/leetcode/TriangleJudgement.sql
@@ -2,4 +2,15 @@
 
 -- The rule for Triangle line is: we need to check if the sum of any two sides is greater than the third side. If this condition holds true for all three combinations of sides, then we can say that a triangle can be formed
 SELECT *, IF(x+y>z AND y+z>x AND z+x>y, "Yes", "No") as Triangle
-FROM Triangle 
+FROM Triangle
+
+/** Schema
+
+*/
+
+    ---------
+
+/** I/O
+    Input:
+
+*/
