4e1459a5d38bb3e62ed9044eb7ee98a8186df809
Henry <ricardophp29@gmail.com>
2023-07-08 16:12:15 -0300

    [LC] (SQL) - Biggest Single Number: Schema

--- src/main/sql/leetcode/BiggestSingleNumber.sql
+++ src/main/sql/leetcode/BiggestSingleNumber.sql
@@ -1,1 +1,16 @@
 -- https://leetcode.com/problems/biggest-single-number/description/
+
+
+/** Schema
+    Table: MyNumbers
+
+    +-------------+------+
+    | Column Name | Type |
+    +-------------+------+
+    | num         | int  |
+    +-------------+------+
+    There is no primary key for this table. It may contain duplicates.
+    Each row of this table contains an integer.
+*/
+
+#---------
