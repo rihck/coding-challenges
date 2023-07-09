5acdb9a2f08a8c7562e385893ca4031cbf5ec170
Henry <ricardophp29@gmail.com>
2023-07-09 16:19:14 -0300

    [LC] (SQL) - Swap Salary: Schema

--- src/main/sql/leetcode/ SwapSalary.sql
+++ src/main/sql/leetcode/ SwapSalary.sql
@@ -1,1 +1,25 @@
 -- https://leetcode.com/problems/swap-salary/description/
+
+
+/** Schema
+    Table: Salary
+
+    +-------------+----------+
+    | Column Name | Type     |
+    +-------------+----------+
+    | id          | int      |
+    | name        | varchar  |
+    | sex         | ENUM     |
+    | salary      | int      |
+    +-------------+----------+
+    id is the primary key for this table.
+    The sex column is ENUM value of type ('m', 'f').
+    The table contains information about an employee.
+*/
+
+#---------
+
+/** I/O
+    Input:
+
+*/
