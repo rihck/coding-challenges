0ed0f96374f638df42bdaf69dd2a6a2be217a7e4
Henry <ricardophp29@gmail.com>
2023-07-09 15:05:53 -0300

    [LC] (SQL) - Not Boring Movies: Schema

--- src/main/sql/leetcode/NotBoringMovies.sql
+++ src/main/sql/leetcode/NotBoringMovies.sql
@@ -1,1 +1,24 @@
 -- https://leetcode.com/problems/not-boring-movies/
+
+/** Schema
+    Table: Cinema
+
+    +----------------+----------+
+    | Column Name    | Type     |
+    +----------------+----------+
+    | id             | int      |
+    | movie          | varchar  |
+    | description    | varchar  |
+    | rating         | float    |
+    +----------------+----------+
+    id is the primary key for this table.
+    Each row contains information about the name of a movie, its genre, and its rating.
+    rating is a 2 decimal places float in the range [0, 10]
+*/
+
+#---------
+
+/** I/O
+
+
+*/
