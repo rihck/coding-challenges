6e7c09ff60da1bf33dc9a6f49e3f9c189f7c3ef2
Henry <ricardophp29@gmail.com>
2023-07-09 15:12:47 -0300

    [LC] (SQL) - Not Boring Movies: Another Approach

--- src/main/sql/leetcode/NotBoringMovies.sql
+++ src/main/sql/leetcode/NotBoringMovies.sql
@@ -7,6 +7,13 @@ WHERE id % 2 != 0
 AND description != 'boring'
 ORDER BY rating DESC
 
+# Another approach beating ~ 23%
+SELECT *
+FROM cinema
+WHERE (id % 2 = 1)
+AND (description <> 'boring')
+ORDER BY rating DESC
+
 /** Schema
     Table: Cinema
 
