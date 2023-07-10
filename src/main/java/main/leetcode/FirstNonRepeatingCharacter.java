6375998ff955681ff571ac2c985125bcf91606e3
Henry <ricardophp29@gmail.com>
2023-07-10 10:34:26 -0300

    [LC] (ALGO) - FirstNonRepeatingCharacter

---
+++ src/main/java/main/leetcode/FirstNonRepeatingCharacter.java
@@ -0,0 +1,16 @@
+package main.leetcode;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class FirstNonRepeatingCharacter {
+
+    public String firstNonRepeatingCharacter(String s){
+        return "";
+    }
+
+
+}
