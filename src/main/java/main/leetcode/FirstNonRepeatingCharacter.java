b1a2fe47dd1d874b3ae14bbf8d8d85838b2e230e
Henry <ricardophp29@gmail.com>
2023-07-10 10:36:41 -0300

    [LC] (ALGO) - FirstNonRepeatingCharacter: Solution -> {TC: O(N)} {SC: O(N)}

--- src/main/java/main/leetcode/FirstNonRepeatingCharacter.java
+++ src/main/java/main/leetcode/FirstNonRepeatingCharacter.java
@@ -9,8 +9,28 @@ import static org.junit.Assert.*;
 public class FirstNonRepeatingCharacter {
 
     public String firstNonRepeatingCharacter(String s){
+        if (s == null || s.isEmpty()) return "";
+
+        String loweredCase = s.toLowerCase();
+        LinkedHashMap<Character, Integer> counters = new LinkedHashMap<>();
+        for (int i = 0; i < loweredCase.length(); i++){
+            Character actualChar = loweredCase.charAt(i);
+            if (!counters.containsKey(loweredCase.charAt(i))){
+                counters.put(loweredCase.charAt(i), 0);
+            }
+
+            counters.put(actualChar, counters.get(actualChar) + 1);
+        }
+
+        Set<Map.Entry<Character, Integer>> entries = counters.entrySet();
+        for (Map.Entry<Character, Integer> entry : entries) {
+            if (entry.getValue() == 1)
+                return String.valueOf(entry.getKey());
+        }
+
         return "";
     }
+
     @Test
     public void emptyOrNullString(){
         assertEquals("", firstNonRepeatingCharacter(""));
