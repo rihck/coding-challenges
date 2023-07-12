5de3ca151d03ed7c6d45665417fb0e705b589db1
Henry <ricardophp29@gmail.com>
2023-07-12 15:25:32 -0300

    [LC] (SQL) - Actors and Directors Who Cooperated At Least Three Times : Solution 1 did not work

--- src/main/sql/leetcode/ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes.sql
+++ src/main/sql/leetcode/ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes.sql
@@ -1,7 +1,9 @@
 -- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
 
+-- Did not work after adding the HAVING at the end.
 SELECT actor_id, director_id
 FROM ActorDirector
 WHERE actor_id = director_id
 GROUP by actor_id
 HAVING count(*) > 2
+
