eb78423483141255ff93c844977a01ace3942825
Henry <ricardophp29@gmail.com>
2023-07-12 15:21:37 -0300

    [LC] (SQL) - Actors and Directors Who Cooperated At Least Three Times : Solution 1 did not work

--- src/main/sql/leetcode/ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes.sql
+++ src/main/sql/leetcode/ActorsAndDirectorsWhoCooperatedAtLeastThreeTimes.sql
@@ -1,1 +1,7 @@
 -- https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/
+
+SELECT actor_id, director_id
+FROM ActorDirector
+WHERE actor_id = director_id
+GROUP by actor_id
+HAVING count(*) > 2
