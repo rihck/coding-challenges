94f87281e5d37472c717ca28970d36b3bddd70c9
Henry <ricardophp29@gmail.com>
2023-07-16 13:41:23 -0300

    [LC] (SQL) - Project Employees I: I/O

--- src/main/sql/leetcode/ProjectEmployees1.sql
+++ src/main/sql/leetcode/ProjectEmployees1.sql
@@ -30,5 +30,35 @@
 #---------
 
 /** I/O
+    Example 1:
+
+    Input:
+    Project table:
+    +-------------+-------------+
+    | project_id  | employee_id |
+    +-------------+-------------+
+    | 1           | 1           |
+    | 1           | 2           |
+    | 1           | 3           |
+    | 2           | 1           |
+    | 2           | 4           |
+    +-------------+-------------+
+    Employee table:
+    +-------------+--------+------------------+
+    | employee_id | name   | experience_years |
+    +-------------+--------+------------------+
+    | 1           | Khaled | 3                |
+    | 2           | Ali    | 2                |
+    | 3           | John   | 1                |
+    | 4           | Doe    | 2                |
+    +-------------+--------+------------------+
+    Output:
+    +-------------+---------------+
+    | project_id  | average_years |
+    +-------------+---------------+
+    | 1           | 2.00          |
+    | 2           | 2.50          |
+    +-------------+---------------+
+    Explanation: The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
 
 */
