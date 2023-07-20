db4cd7540d755ec43740865b5bb6dd703721e37a
Henry <ricardophp29@gmail.com>
2023-07-20 09:07:20 -0300

    [LC] (SQL) - Recyclable and Low Fat Products

--- src/main/sql/leetcode/RecyclableAndLowFatProducts.sql
+++ src/main/sql/leetcode/RecyclableAndLowFatProducts.sql
@@ -1,1 +1,16 @@
 -- https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+
+/** Schema
+    Table: Products
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | product_id  | int     |
+    | low_fats    | enum    |
+    | recyclable  | enum    |
+    +-------------+---------+
+    In SQL, product_id is the primary key for this table.
+    low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+    recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+ */
