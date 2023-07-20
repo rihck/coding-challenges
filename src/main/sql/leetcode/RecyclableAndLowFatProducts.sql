5925897a3a7c71cbea9e625ae9d4251a91ecf04b
Henry <ricardophp29@gmail.com>
2023-07-20 09:09:47 -0300

    [LC] (SQL) - Recyclable and Low Fat Products

--- src/main/sql/leetcode/RecyclableAndLowFatProducts.sql
+++ src/main/sql/leetcode/RecyclableAndLowFatProducts.sql
@@ -1,5 +1,10 @@
 -- https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
 
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y'
+AND recyclable = 'Y'
+
 /** Schema
     Table: Products
 
