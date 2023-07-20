4fb7fff9692fbe49461b41dd5356a83e3b02ccf1
Henry <ricardophp29@gmail.com>
2023-07-20 09:07:43 -0300

    [LC] (SQL) - Recyclable and Low Fat Products

--- src/main/sql/leetcode/RecyclableAndLowFatProducts.sql
+++ src/main/sql/leetcode/RecyclableAndLowFatProducts.sql
@@ -14,3 +14,26 @@
     low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
     recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
  */
+
+/** I/O
+    Example 1:
+    Input:
+    Products table:
+    +-------------+----------+------------+
+    | product_id  | low_fats | recyclable |
+    +-------------+----------+------------+
+    | 0           | Y        | N          |
+    | 1           | Y        | Y          |
+    | 2           | N        | Y          |
+    | 3           | Y        | Y          |
+    | 4           | N        | N          |
+    +-------------+----------+------------+
+    Output:
+    +-------------+
+    | product_id  |
+    +-------------+
+    | 1           |
+    | 3           |
+    +-------------+
+    Explanation: Only products 1 and 3 are both low fat and recyclable.
+ */
