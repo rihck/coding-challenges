024d7d282804e4098603163d426f1fcaed8243dc
Henry <ricardophp29@gmail.com>
2023-07-21 09:33:43 -0300

    [LC] (ALGO) - 1071. Greatest Common Divisor of Strings

---
+++ src/main/java/main/leetcode/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,8 @@
+package main.leetcode;
+
+/**
+ * https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
+ */
+public class GreatestCommonDivisorOfStrings {
+
+}
