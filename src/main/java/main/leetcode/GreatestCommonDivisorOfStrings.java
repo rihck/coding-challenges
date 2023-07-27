8c450919cd3601743bff5c0012c8ff2c5ee2db26
Henry <ricardophp29@gmail.com>
2023-07-27 10:28:49 -0300

    [LC] (ALGO) - 1071. Greatest Common Divisor of Strings: First approach (not working)

--- src/main/java/main/leetcode/GreatestCommonDivisorOfStrings.java
+++ src/main/java/main/leetcode/GreatestCommonDivisorOfStrings.java
@@ -1,10 +1,51 @@
 package main.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
  */
 public class GreatestCommonDivisorOfStrings {
     public String gcdOfStrings(String str1, String str2) {
+        String incrementalResult = "";
+        int pointer = 0;
+        //"ABCABC", "ABC"
+        //"ABABAB", "ABAB"
 
+        for (int i = 0; i < str1.length(); i++){
+            Character currentChar = str1.charAt(i);
+
+            if (pointer >= str2.length())
+                pointer = 0;
+
+            Character charAtPointer = str2.charAt(pointer);
+
+            if (currentChar == charAtPointer){
+                incrementalResult += currentChar;
+                pointer++;
+            }
+            else {
+                pointer = 0;
+                str2 = incrementalResult;
+                incrementalResult = "";
+            }
+
+        }
+
+        return incrementalResult;
     }
+
+    @Test
+    public void perfectDivision(){
+        assertEquals("ABC", gcdOfStrings("ABCABC", "ABC"));
+    }
+
+//    @Test
+//    public void divisionButNoFullMatch(){
+//        assertEquals("AB", gcdOfStrings("ABABAB", "ABAB"));
+//    }
+
 }
