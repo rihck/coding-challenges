4e7ec6b7032e43ab7cc0a5c8b8fceb4749d65b8a
Henry <ricardophp29@gmail.com>
2023-07-27 10:45:45 -0300

    [LC] (SQL) - 1683. Invalid Tweets

--- src/main/sql/leetcode/InvalidTweets.sql
+++ src/main/sql/leetcode/InvalidTweets.sql
@@ -1,2 +1,41 @@
 -- https://leetcode.com/problems/invalid-tweets/?envType=study-plan-v2&envId=top-sql-50
 
+/**
+  Schema
+
+  Table: Tweets
+
+    +----------------+---------+
+    | Column Name    | Type    |
+    +----------------+---------+
+    | tweet_id       | int     |
+    | content        | varchar |
+    +----------------+---------+
+    In SQL, tweet_id is the primary key for this table.
+    This table contains all the tweets in a social media app.
+ */
+
+/**
+  I/O
+
+  Example 1:
+
+    Input:
+    Tweets table:
+    +----------+----------------------------------+
+    | tweet_id | content                          |
+    +----------+----------------------------------+
+    | 1        | Vote for Biden                   |
+    | 2        | Let us make America great again! |
+    +----------+----------------------------------+
+    Output:
+    +----------+
+    | tweet_id |
+    +----------+
+    | 2        |
+    +----------+
+    Explanation:
+    Tweet 1 has length = 14. It is a valid tweet.
+    Tweet 2 has length = 32. It is an invalid tweet.
+
+ */
