386b41610d254148a164941eaee989128c1f1333
Henry <ricardophp29@gmail.com>
2023-07-28 12:25:27 -0300

    [LC] (SQL) - 1378. Replace Employee ID With The Unique Identifier

--- src/main/sql/leetcode/ReplaceEmployeeIDWithTheUniqueIdentifier.sql
+++ src/main/sql/leetcode/ReplaceEmployeeIDWithTheUniqueIdentifier.sql
@@ -1,5 +1,9 @@
 -- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/?envType=study-plan-v2&envId=top-sql-50
 
+SELECT unique_id, name
+FROM Employees e
+LEFT JOIN EmployeeUNI eu ON (e.id = eu.id)
+
 /**
   Schema
 
