065dad1cb436bf66714a1b8abb958842037e5ed2
Henry <ricardophp29@gmail.com>
2023-07-28 12:21:38 -0300

    [LC] (SQL) - 1378. Replace Employee ID With The Unique Identifier

--- src/main/sql/leetcode/ReplaceEmployeeIDWithTheUniqueIdentifier.sql
+++ src/main/sql/leetcode/ReplaceEmployeeIDWithTheUniqueIdentifier.sql
@@ -1,2 +1,37 @@
 -- https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/?envType=study-plan-v2&envId=top-sql-50
 
+/**
+  Schema
+
+  Table: Employees
+
+    +---------------+---------+
+    | Column Name   | Type    |
+    +---------------+---------+
+    | id            | int     |
+    | name          | varchar |
+    +---------------+---------+
+    In SQL, id is the primary key for this table.
+    Each row of this table contains the id and the name of an employee in a company.
+
+
+    Table: EmployeeUNI
+
+    +---------------+---------+
+    | Column Name   | Type    |
+    +---------------+---------+
+    | id            | int     |
+    | unique_id     | int     |
+    +---------------+---------+
+    In SQL, (id, unique_id) is the primary key for this table.
+    Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+
+ */
+
+/**
+  I/O
+
+
+
+ */
