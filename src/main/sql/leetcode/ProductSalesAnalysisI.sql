75175b5e8f211431dbe4e2d622281bc742fb07f5
Henry <ricardophp29@gmail.com>
2023-07-31 09:47:17 -0300

    [LC] (SQL) - 1068. Product Sales Analysis I

--- src/main/sql/leetcode/ProductSalesAnalysisI.sql
+++ src/main/sql/leetcode/ProductSalesAnalysisI.sql
@@ -1,1 +1,39 @@
 -- https://leetcode.com/problems/product-sales-analysis-i/?envType=study-plan-v2&envId=top-sql-50
+
+/**
+  Schema
+
+  Table: Sales
+    +-------------+-------+
+    | Column Name | Type  |
+    +-------------+-------+
+    | sale_id     | int   |
+    | product_id  | int   |
+    | year        | int   |
+    | quantity    | int   |
+    | price       | int   |
+    +-------------+-------+
+    (sale_id, year) is the primary key of this table.
+    product_id is a foreign key to Product table.
+    Each row of this table shows a sale on the product product_id in a certain year.
+    Note that the price is per unit.
+
+
+    Table: Product
+
+    +--------------+---------+
+    | Column Name  | Type    |
+    +--------------+---------+
+    | product_id   | int     |
+    | product_name | varchar |
+    +--------------+---------+
+    product_id is the primary key of this table.
+    Each row of this table indicates the product name of each product.
+
+ */
+
+
+/**
+  I/O
+
+ */
