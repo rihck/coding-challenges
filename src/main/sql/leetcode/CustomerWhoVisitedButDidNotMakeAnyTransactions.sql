92ae8c8a1c446b0cbae5947fccfc97857b304a3d
Henry <ricardophp29@gmail.com>
2023-09-20 12:15:45 -0300

    [LC] (SQL) - 1581. Customer Who Visited but Did Not Make Any Transactions

--- src/main/sql/leetcode/CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
+++ src/main/sql/leetcode/CustomerWhoVisitedButDidNotMakeAnyTransactions.sql
@@ -1,2 +1,28 @@
 -- https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/?envType=study-plan-v2&envId=top-sql-50
 
+/** Schema
+    Table: Visits
+
+    +-------------+---------+
+    | Column Name | Type    |
+    +-------------+---------+
+    | visit_id    | int     |
+    | customer_id | int     |
+    +-------------+---------+
+    visit_id is the column with unique values for this table.
+    This table contains information about the customers who visited the mall.
+
+
+    Table: Transactions
+
+    +----------------+---------+
+    | Column Name    | Type    |
+    +----------------+---------+
+    | transaction_id | int     |
+    | visit_id       | int     |
+    | amount         | int     |
+    +----------------+---------+
+    transaction_id is column with unique values for this table.
+    This table contains information about the transactions made during the visit_id.
+ */
+
