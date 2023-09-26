a7c8db761bbe56073ed36609b7b53fbe19f69ac8
Henry <ricardophp29@gmail.com>
2023-09-26 12:59:50 -0300

    [LC] (SQL) - 1661. Average Time of Process per Machine

--- src/main/sql/leetcode/AverageTimeOfProcessPerMachine.sql
+++ src/main/sql/leetcode/AverageTimeOfProcessPerMachine.sql
@@ -1,5 +1,13 @@
 -- https://leetcode.com/problems/average-time-of-process-per-machine/description/?envType=study-plan-v2&envId=top-sql-50
 
+-- SelfJoin solution
+SELECT a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) as processing_time
+FROM Activity a1
+ INNER JOIN Activity a2 ON
+     (a2.machine_id = a1.machine_id AND a1.process_id = a2.process_id) AND
+     (a1.activity_type = 'start' AND a2.activity_type = 'end')
+GROUP BY a1.machine_id;
+
 /** Schema
 
   Table: Activity
