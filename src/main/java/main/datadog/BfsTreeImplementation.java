d077fd032ce0b6d1d48b7f8171e04604f856ade0
Henry <ricardophp29@gmail.com>
2023-11-24 13:04:29 -0300

    [LC] (DataDog) - BfsTreeImplementation

---
+++ src/main/java/main/datadog/BfsTreeImplementation.java
@@ -0,0 +1,9 @@
+package main.datadog;
+
+public class BfsTreeImplementation {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/main/datadog/Node.java
@@ -0,0 +1,44 @@
+package main.datadog;
+
+public class Node { String value;
+   Node right;
+   Node left;
+
+    public Node(String value){
+        this.value = value;
+    }
+
+    public Node(String value, Node left, Node right){
+        this.value = value;
+        this.right = right;
+        this.left = left;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value='" + value + '\'' +
+                ", right=" + right +
+                ", left=" + left +
+                '}';
+    }
+
+    public Node createDefaultTree(){
+        //         a
+//          b              c
+//      d      e        f     g
+
+        Node d = new Node("d");
+        Node e = new Node("e");
+
+        Node f = new Node("f");
+        Node g = new Node("g");
+
+        Node b = new Node("b", d, e);
+        Node c = new Node("c", f, g);
+
+        Node a = new Node("a", b, c);
+
+        return a;
+    }
+}
