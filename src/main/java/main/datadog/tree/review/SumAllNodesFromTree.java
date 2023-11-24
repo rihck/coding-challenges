44f2daa9c3f4f70b6e0ff43b2c0e6a3f01565f53
Henry <ricardophp29@gmail.com>
2023-11-24 14:04:56 -0300

    [LC] (DataDog) - findSumOfAllNodes iterative

--- src/main/java/main/datadog/tree/review/SumAllNodesFromTree.java
+++ src/main/java/main/datadog/tree/review/SumAllNodesFromTree.java
@@ -1,16 +1,37 @@
 package main.datadog.tree.review;
 
+import java.util.Stack;
+
 public class SumAllNodesFromTree {
     public static void main(String[] args) {
         Node<Integer> root = Node.createDefaultIntegerTree();
         Integer result = findSumOfAllNodes(root);
         System.out.println(result);
+
+        Integer result2 = findSumOfAllNodesIterative(root);
+        System.out.println(result2);
     }
 
     private static Integer findSumOfAllNodes(Node<Integer> root) {
         if (root == null) return 0;
 
         return root.value + findSumOfAllNodes(root.left) + findSumOfAllNodes(root.right);
+    }
+
+    private static Integer findSumOfAllNodesIterative(Node<Integer> root) {
+        Stack<Node<Integer>> nodesStack = new Stack<>();
+        nodesStack.push(root);
+
+        int sum = 0;
+        while (!nodesStack.empty()) {
+            Node<Integer> current = nodesStack.pop();
+            sum += current.value;
+
+            if (current.right != null) nodesStack.push(current.right);
+            if (current.left != null) nodesStack.push(current.left);
+        }
+
+        return sum;
     }
 
 }
