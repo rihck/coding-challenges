396296e5fc9bf10e5a4157ef855c9a8a6ca3d8dc
Henry <ricardophp29@gmail.com>
2023-11-24 13:45:41 -0300

    [LC] (DataDog) - FindValueInTreeIterative

--- src/main/java/main/datadog/tree/review/FindValueInTreeRecursive.java
+++ src/main/java/main/datadog/tree/review/FindValueInTreeRecursive.java
@@ -1,14 +1,25 @@
 package main.datadog.tree.review;
 
+import java.util.Stack;
+
 public class FindValueInTreeRecursive {
     public static void main(String[] args) {
 
+        // Recursive
         Node root = Node.createDefaultTree();
         boolean result = findValueInTree(root, "e");
         System.out.println(result);
 
         boolean result2 = findValueInTree(root, "w");
         System.out.println(result2);
+
+        // Iterative
+        boolean result3 = findValueInTreeUsingStack(root, "e");
+        System.out.println(result3);
+
+        boolean result4 = findValueInTreeUsingStack(root, "w");
+        System.out.println(result4);
+
     }
 
     public static boolean findValueInTree(Node node, String target){
@@ -20,5 +31,21 @@ public class FindValueInTreeRecursive {
         if (node.value.equals(target)) return true;
 
         return findValueInTree(node.left, target) || findValueInTree(node.right, target);
+    }
+
+    public static boolean findValueInTreeUsingStack(Node node, String target) {
+        Stack<Node> nodes = new Stack<>();
+        nodes.push(node);
+
+        while (!nodes.empty()) {
+            Node current = nodes.pop();
+
+            if (current.value.equals(target)) return true;
+
+            if (current.right != null) nodes.push(current.right);
+            if (current.left != null) nodes.push(current.left);
+        }
+
+        return false;
     }
 }
