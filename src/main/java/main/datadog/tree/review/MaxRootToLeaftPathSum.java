a85ded094af49d6fce3e14e93e9ac35918424ce2
Henry <ricardophp29@gmail.com>
2023-11-28 11:45:28 -0300

    [LC] (DataDog) - maxRootToLeaf iterative

--- src/main/java/main/datadog/tree/review/MaxRootToLeaftPathSum.java
+++ src/main/java/main/datadog/tree/review/MaxRootToLeaftPathSum.java
@@ -1,10 +1,13 @@
 package main.datadog.tree.review;
 
+import java.util.Stack;
+
 public class MaxRootToLeaftPathSum {
     public static void main(String[] args) {
         Node<Integer> root = Node.createIntegerTreeToCalculateRootToLeaft();
 
         System.out.println(maxRootToLeaft(root));
+        System.out.println(maxRootToLeaftStack(root));
     }
 
     public static Integer maxRootToLeaft(Node<Integer> node){
@@ -12,5 +15,35 @@ public class MaxRootToLeaftPathSum {
         if (node.right == null && node.left == null) return node.value;
 
         return node.value + Math.max(maxRootToLeaft(node.left), maxRootToLeaft(node.right));
+    }
+
+    public static Integer maxRootToLeaftStack(Node<Integer> node) {
+        Stack<Node<Integer>> nodeStack = new Stack<>();
+        Stack<Integer> sumStack = new Stack<>();
+
+        nodeStack.push(node);
+        sumStack.push(node.value);
+        int maxSum = 0;
+
+        while (!nodeStack.empty()) {
+            Node<Integer> currentNode = nodeStack.pop();
+            Integer currentSum = sumStack.pop();
+
+            if (currentNode.right == null && currentNode.left == null)
+                maxSum = Math.max(maxSum, currentSum);
+
+            if (currentNode.right != null) {
+                nodeStack.push(currentNode.right);
+                sumStack.push(currentSum + currentNode.right.value);
+            }
+
+            if (currentNode.left != null) {
+                nodeStack.push(currentNode.left);
+                sumStack.push(currentSum + currentNode.left.value);
+            }
+
+        }
+
+        return maxSum;
     }
 }
